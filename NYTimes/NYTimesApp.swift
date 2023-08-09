0efe77f2c599c2f5fd3f7e7fd94a11b5bdf50cc2
Nisar Ahmad <nisar.ahmed@croeminc.com>
2023-08-09 09:41:03 +0500

    Initial Commit

---
+++ NYTimes/NYTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTimesApp.swift
+//  NYTimes
+//
+//  Created by Nisar Ahmad on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct NYTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
